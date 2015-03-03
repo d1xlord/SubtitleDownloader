f4dfb12f5e4685e323e5122111298a95783f45c2
Nafis Ahmed <nafubd@gmail.com>
2015-03-03 12:02:21 +0600

    made some changes regarding codes

--- src/com/googlecode/opensubtitlesjapi/OpenSubtitlesAPI.java
+++ src/com/googlecode/opensubtitlesjapi/OpenSubtitlesAPI.java
@@ -1,20 +1,3 @@
-/*
- *  Copyright 2011 daniele.belletti@gmail.com.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *  under the License.
- */
-
 package com.googlecode.opensubtitlesjapi;
 
 import java.io.BufferedOutputStream;
@@ -26,7 +9,6 @@ import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,14 +19,10 @@ import org.apache.ws.commons.util.Base64;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 
-/**
- *
- * @author daniele.belletti@gmail.com
- */
 public class OpenSubtitlesAPI {
 
   private static final Logger log = Logger.getLogger(OpenSubtitlesAPI.class);
-  private static final String USER_AGENT = "OSTestUserAgent";
+  private static final String USER_AGENT = "OSTestUserAgent"; // Testing UA, TODO: get my own UA
   //private static final String USER_AGENT = "OS Test User Agent";
   private static final String END_POINT = "http://api.opensubtitles.org/xml-rpc";
   private static final String LANGUAGE = "en";
--- src/com/googlecode/opensubtitlesjapi/TypeFactory.java
+++ src/com/googlecode/opensubtitlesjapi/TypeFactory.java
@@ -1,20 +1,3 @@
-/*
- *  Copyright 2011 daniele.belletti@gmail.com.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *  under the License.
- */
-
 package com.googlecode.opensubtitlesjapi;
 
 import org.apache.xmlrpc.common.TypeFactoryImpl;
@@ -23,10 +6,6 @@ import org.apache.xmlrpc.common.XmlRpcStreamConfig;
 import org.apache.xmlrpc.serializer.TypeSerializer;
 import org.xml.sax.SAXException;
 
-/**
- *
- * @author daniele.belletti@gmail.com
- */
 public class TypeFactory extends TypeFactoryImpl {
 
   public TypeFactory(XmlRpcController pController) {
